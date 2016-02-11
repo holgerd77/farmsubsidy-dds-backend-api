75c10702ec542ee4e1ed4f8c9eb26242e89ae5d3
holgerd77 <Holger.Drewes@googlemail.com>
2016-02-11 13:57:08 +0100

    Added countries API endpoint

--- app.py
+++ app.py
@@ -10,18 +10,19 @@ app.config.from_object(default_settings)
 cors = CORS(app)
 
 PAYMENTS_ENDPOINT = '/{version}/payments/'.format(version=app.config['API_VERSION'])
+COUNTRIES_ENDPOINT = '/{version}/countries/'.format(version=app.config['API_VERSION'])
 
 
 def build_query_dict(request):
     q_dict = {
-        #'sort': [ { 'amount_euro': 'desc' } ],
+        'sort': [ { 'amount_euro': 'desc' } ],
         'query': {
             'bool': { 'must': [], 'filter': [], } },
         'aggs': {
             'Towns': { 'terms' : { 'field' : 'town', 'size': 5 } },
             'Countries': { 'terms' : { 'field' : 'country', 'size': 5 } },
             'Years': { 'terms' : { 'field' : 'year', 'size': 5 } },
-            'Sub Payments Type': { 'terms' : { 'field' : 'sub_payments_euro.name', 'size': 5 } }
+            'Sub Payments Type': { 'terms' : { 'field' : 'sub_payments_euro.name', 'size': 12 } }
         }
     }
     m = q_dict['query']['bool']['must']
@@ -52,7 +53,7 @@ def build_query_dict(request):
         f.append({ 'term': { 'year': int(year) }})
     
     amount_euro_gte = request.args.get('amount_euro_gte', '')
-    if amount_euro_gte:
+    if amount_euro_gte != '':
         f.append({ 'range': { 'amount_euro': { 'gte': float(amount_euro_gte) } }})
     
     sub_payments_type = request.args.get('sub_payments_type', '')
@@ -63,16 +64,34 @@ def build_query_dict(request):
     
 
 @app.route(PAYMENTS_ENDPOINT, methods=['GET',])
-def index():
+def payments():
     #q = request.args.get('q', '')
     q_dict = build_query_dict(request)
     payload = { 'source': json.dumps(q_dict), 'pretty': '' }
+    from_attr = request.args.get('start', '')
+    if from_attr != '':
+        payload['from'] = from_attr;
+    size = request.args.get('rows', '')
+    if size != '':
+        payload['size'] = size;
+    
     r = requests.get(app.config['ELASTIC_URL'] + '_search', params=payload)
     if True or r.status_code == 200:
         return make_response(r.content, r.status_code)
     else:
         e_dict = { 'error': { 'msg': 'An error occured', 'code': r.status_code } }
         return make_response(jsonify(e_dict), r.status_code)
+
+
+@app.route(COUNTRIES_ENDPOINT, methods=['GET',])
+def countries():
+    try:
+        with open('countries.json', 'r') as c_file:
+            c_str = c_file.read()
+            return make_response(c_str, 200)
+    except EnvironmentError:
+        e_dict = { 'error': { 'msg': 'An error occured', 'code': 500 } }
+        return make_response(jsonify(e_dict), 500)
 
 
 @app.errorhandler(404)
